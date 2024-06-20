f8c0574ee854ac8f1bf5411d9f4bfa39fa17d67c
vipma <vipma@DESKTOP-HGCETD5>
2024-06-20 17:01:56 +0100

    Add project files.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	unsigned short int num1;
+
+	cout << "Please enter a number: ";  cin >> num1; cout << "\n";
+
+
+	cout << " Number^2 = " << num1 * num1 << endl;
+	cout << " Number^3 = " << num1 * num1 * num1 << endl;
+	cout << " Number^4 = " << num1 * num1 * num1 * num1 << endl;
+}
